38b6e14e5c20975987b87123254a8fcd1ddb94d0
Sathiya R <rskeerthi01@gmail.com>
2026-06-29 23:30:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 function makeid(l) {
+	var res="";
+	var char_str="qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890";
+	for(let i=0;i<l;i++){
+		res+=char_str.charAt(
+			Math.floor(
+				Math.random()* char_str.length
+			)
+				
+			)
+		}return res;
+	}
   // write your code here
 }
 
