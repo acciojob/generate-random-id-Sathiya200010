368a98d218500019faaaa557124391d3bde10d40
Sathiya R <rskeerthi01@gmail.com>
2026-06-29 23:33:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
 function makeid(l) {
 	var res="";
 	var char_str="qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890";
-	for(let i=0;i<l;i++){
-		res+=char_str.charAt(
-			Math.floor(
-				Math.random()* char_str.length
-			)
-				
-			)
+	for(var i=0;i<l;i++){
+		res += char_str.charAt(Math.floor(Math.random()*char_str.length));
 		}return res;
 	}
   // write your code here
